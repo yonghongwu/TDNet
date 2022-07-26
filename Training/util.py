a68d3a45f33f682e7dde8e485ae222d371df8646
wuyonghuang <1303635317@qq.com>
2022-07-26 21:16:25 +0800

    add

--- Training/util.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def calculate_dice(input_log, input_lab):
-    intersection = input_log * input_lab
-    return (2 * np.sum(intersection) + 1e-6) / (np.sum(input_log) + np.sum(input_lab) + 1e-6)
